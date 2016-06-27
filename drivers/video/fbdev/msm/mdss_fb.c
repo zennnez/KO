147ef497cb2a2970c2fdce015eeff53be95d9ed4
flar2 <asegaert@gmail.com>
2016-06-26 23:06:11 -0400

    mdss_fb: add backlight dimmer option

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -88,6 +88,12 @@ int backlight_max = 255;
 module_param(backlight_min, int, 0755);
 module_param(backlight_max, int, 0755);
 
+#define MDSS_BRIGHT_TO_BL_DIM(out, v) do {\
+			out = (12*v*v+1393*v+3060)/4465;\
+			} while (0)
+bool backlight_dimmer = false;
+module_param(backlight_dimmer, bool, 0755);
+
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
 
