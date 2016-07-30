3804c9b6752fcdef84f764ffe23cc693a2578740
flar2 <asegaert@gmail.com>
2016-07-30 13:41:05 -0400

    msm_adreno_tz: add adrenoboost parameter
    This scales up way too easily, it needs to be tuned.

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -58,10 +58,7 @@ static DEFINE_SPINLOCK(suspend_lock);
 
 #define TAG "msm_adreno_tz: "
 
-#if 1
-static unsigned int adrenoboost = 2;
-#endif
-
+static unsigned int adrenoboost = 10000;
 static u64 suspend_time;
 static u64 suspend_start;
 static unsigned long acc_total, acc_relative_busy;
@@ -92,7 +89,6 @@ u64 suspend_time_ms(void)
 	return time_diff;
 }
 
-#if 1
 static ssize_t adrenoboost_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
@@ -105,16 +101,17 @@ static ssize_t adrenoboost_show(struct device *dev,
 static ssize_t adrenoboost_save(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
-	unsigned int input;
+	int input;
 	sscanf(buf, "%d ", &input);
-	if (input > 3)
+	if (input < 0 || input > 50000) {
 		adrenoboost = 0;
-	else
+	} else {
 		adrenoboost = input;
+	}
 
 	return count;
 }
-#endif
+
 
 static ssize_t gpu_load_show(struct device *dev,
 		struct device_attribute *attr,
@@ -162,10 +159,8 @@ static ssize_t suspend_time_show(struct device *dev,
 	return snprintf(buf, PAGE_SIZE, "%llu\n", time_diff);
 }
 
-#if 1
 static DEVICE_ATTR(adrenoboost, 0644,
 		adrenoboost_show, adrenoboost_save);
-#endif
 
 static DEVICE_ATTR(gpu_load, 0444, gpu_load_show, NULL);
 
@@ -176,9 +171,7 @@ static DEVICE_ATTR(suspend_time, 0444,
 static const struct device_attribute *adreno_tz_attr_list[] = {
 		&dev_attr_gpu_load,
 		&dev_attr_suspend_time,
-#if 1
 		&dev_attr_adrenoboost,
-#endif
 		NULL
 };
 
@@ -538,7 +531,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 #else
 		scm_data[0] = level;
 		scm_data[1] = priv->bin.total_time;
-		scm_data[2] = priv->bin.busy_time;
+		scm_data[2] = priv->bin.busy_time + (level * adrenoboost);
 		scm_data[3] = context_count;
 		__secure_tz_update_entry3(scm_data, sizeof(scm_data),
 					&val, sizeof(val), priv);
