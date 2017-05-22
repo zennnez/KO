b27a6a6d96cc0108dfbc6578f5002b360774c7df
Sultanxda <sultanxda@gmail.com>
2017-05-22 15:41:38 -0700

    proc: Remove additional SafetyNet flags from /proc/cmdline
    SafetyNet checks androidboot.veritymode in Nougat, so remove it.
    
    Additionally, remove androidboot.enable_dm_verity and androidboot.secboot
    in case SafetyNet will check them in the future.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: andip71 <andreasp@gmx.de>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Conflicts:
    	fs/proc/cmdline.c

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -39,6 +39,7 @@ static void remove_flag(char *cmd, const char *flag)
 }
 
 static void remove_safetynet_flags(char *cmd)
+
 {
 	remove_flag(cmd, "androidboot.enable_dm_verity=");
 	remove_flag(cmd, "androidboot.secboot=");
@@ -46,31 +47,16 @@ static void remove_safetynet_flags(char *cmd)
 	remove_flag(cmd, "androidboot.veritymode=");
 }
 
+
 static int __init proc_cmdline_init(void)
 {
-
-	char *offset_addr, *cmd = new_command_line;
-
-	strcpy(cmd, saved_command_line);
+	strcpy(new_command_line, saved_command_line);
 
 	/*
-	 * Remove 'androidboot.verifiedbootstate' flag from command line seen
-	 * by userspace in order to pass SafetyNet CTS check.
+	 * Remove various flags from command line seen by userspace in order to
+	 * pass SafetyNet CTS check.
 	 */
-	offset_addr = strstr(cmd, "androidboot.verifiedbootstate=");
-	if (offset_addr) {
-		size_t i, len, offset;
-
-		len = strlen(cmd);
-		offset = offset_addr - cmd;
-
-		for (i = 1; i < (len - offset); i++) {
-			if (cmd[offset + i] == ' ')
-				break;
-		}
-
-		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
-	}
+	remove_safetynet_flags(new_command_line);
 
 
 
