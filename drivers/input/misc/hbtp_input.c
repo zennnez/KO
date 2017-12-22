7bda67e779130369845732dbe1a3d01dc1500333
Xin Hua Lu <luxinhua@codeaurora.org>
2017-12-22 17:48:12 +0800

    BACKPORT: hbtp-input : fix L13A electric leakage issue
    Always open touch power to prevent current flow backward
    lead to the leakage.
    
    Signed-off-by: Xin Hua Lu <luxinhua@codeaurora.org>
    Change-Id: If09bf4e55b59bfa98f1cd48cbc051c29ead2b85d
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/input/misc/hbtp_input.c
+++ drivers/input/misc/hbtp_input.c
@@ -1216,12 +1216,6 @@ static int hbtp_fb_suspend(struct hbtp_data *ts)
 			pr_err("%s: failed to disable GPIO pins\n", __func__);
 			goto err_pin_disable;
 		}
-
-		rc = hbtp_pdev_power_on(ts, false);
-		if (rc) {
-			pr_err("%s: failed to disable power\n", __func__);
-			goto err_power_disable;
-		}
 		ts->power_suspended = true;
 	}
 
@@ -1248,8 +1242,7 @@ static int hbtp_fb_suspend(struct hbtp_data *ts)
 
 	mutex_unlock(&hbtp->mutex);
 	return 0;
-err_power_disable:
-	hbtp_pinctrl_enable(ts, true);
+
 err_pin_disable:
 	mutex_unlock(&hbtp->mutex);
 	return rc;
@@ -1270,11 +1263,6 @@ static int hbtp_fb_early_resume(struct hbtp_data *ts)
 			pr_err("%s: power is not suspended\n", __func__);
 			mutex_unlock(&hbtp->mutex);
 			return 0;
-		}
-		rc = hbtp_pdev_power_on(ts, true);
-		if (rc) {
-			pr_err("%s: failed to enable panel power\n", __func__);
-			goto err_power_on;
 		}
 
 		rc = hbtp_pinctrl_enable(ts, true);
@@ -1322,8 +1310,6 @@ static int hbtp_fb_early_resume(struct hbtp_data *ts)
 
 err_pin_enable:
 	hbtp_pdev_power_on(ts, false);
-err_power_on:
-	mutex_unlock(&hbtp->mutex);
 	return rc;
 }
 
@@ -1408,6 +1394,12 @@ static int hbtp_pdev_probe(struct platform_device *pdev)
 			}
 		}
 		hbtp->vcc_dig = vcc_dig;
+	}
+
+	error = hbtp_pdev_power_on(hbtp, true);
+	if (error) {
+		pr_err("%s: failed to power on\n", __func__);
+		return error;
 	}
 
 	return 0;
