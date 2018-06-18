2cd6a42171a4bdc34c10609aab8bb87078fb9f1e
Haseo97 <jawgattus@gmail.com>
2018-06-18 21:30:47 +0700

    x00td: Fix derps and compilations for some govs
    Actually, based on https://github.com/Akianonymus/kernel_harpia/commit/8e2ddc1baf92792e91199f0c56513121a83b726c
    
    We need to do that for mow xD
    
    Signed-off-by: Haseo97 <jawgattus@gmail.com>

--- drivers/cpufreq/cpufreq_darkness.c
+++ drivers/cpufreq/cpufreq_darkness.c
@@ -718,7 +718,6 @@ static int cpufreq_governor_darkness(struct cpufreq_policy *policy,
 		policy->governor_data = tunables;
 		if (!have_governor_per_policy()) {
 			common_tunables = tunables;
-			WARN_ON(cpufreq_get_global_kobject());
 		}
 
 		rc = sysfs_create_group(get_governor_parent_kobj(policy),
@@ -728,7 +727,6 @@ static int cpufreq_governor_darkness(struct cpufreq_policy *policy,
 			policy->governor_data = NULL;
 			if (!have_governor_per_policy()) {
 				common_tunables = NULL;
-				cpufreq_put_global_kobject();
 			}
 			return rc;
 		}
@@ -753,8 +751,6 @@ static int cpufreq_governor_darkness(struct cpufreq_policy *policy,
 			sysfs_remove_group(get_governor_parent_kobj(policy),
 					get_sysfs_attr());
 
-			if (!have_governor_per_policy())
-				cpufreq_put_global_kobject();
 			common_tunables = NULL;
 		}
 
--- drivers/cpufreq/cpufreq_ironactive.c
+++ drivers/cpufreq/cpufreq_ironactive.c
@@ -1563,7 +1563,6 @@ static int cpufreq_governor_ironactive(struct cpufreq_policy *policy,
 		tunables->usage_count = 1;
 		policy->governor_data = tunables;
 		if (!have_governor_per_policy()) {
-			WARN_ON(cpufreq_get_global_kobject());
 			common_tunables = tunables;
 		}
 
@@ -1574,7 +1573,6 @@ static int cpufreq_governor_ironactive(struct cpufreq_policy *policy,
 			policy->governor_data = NULL;
 			if (!have_governor_per_policy()) {
 				common_tunables = NULL;
-				cpufreq_put_global_kobject();
 			}
 			return rc;
 		}
@@ -1601,8 +1599,6 @@ static int cpufreq_governor_ironactive(struct cpufreq_policy *policy,
 
 			sysfs_remove_group(get_governor_parent_kobj(policy),
 					get_sysfs_attr());
-			if (!have_governor_per_policy())
-				cpufreq_put_global_kobject();
 			common_tunables = NULL;
 		}
 
