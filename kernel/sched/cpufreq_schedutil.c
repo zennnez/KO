0229bce7b9ffc5babc95d742fdd8b994771a1f47
Hani <kirkirehani93@gmail.com>
2018-06-20 09:35:41 +0000

    x00td:Enabling few tweaks to reduce idle drain

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -172,13 +172,19 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
  * The lowest driver-supported frequency which is equal or greater than the raw
  * next_freq (as calculated above) is returned, subject to policy min/max and
  * cpufreq driver limitations.
- */
+ * During screen off, the big cluster will be capped to a lower frequency to
+ help with idle drain. */
 static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 				  unsigned long util, unsigned long max)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
 	unsigned int freq = arch_scale_freq_invariant() ?
 				policy->cpuinfo.max_freq : policy->cur;
+
+	const bool display_on = is_display_on();
+	
+	if(!display_on && policy->cpu > 1)
+		freq = freq >> 2;
 
 	freq = (freq + (freq >> 2)) * util / max;
 
