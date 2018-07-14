768c73f9eb915af34026785397035b22f5c4e2a5
sudeep <rishi.gothic@gmail.com>
2018-07-14 22:34:31 +0530

    rename it KO

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=SakhatKernel by Sudeep Duhoon
+kernel.string=AnyKernel2 by osm0sis @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
