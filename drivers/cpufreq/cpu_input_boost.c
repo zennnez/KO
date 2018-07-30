5c06d8da8f349f71ec248f2fefdf37f1066b2c09
Yaroslav Furman <yaro330@gmail.com>
2018-07-30 11:35:21 +0300

    cpu_input_boost: don't boost big cluster when there are no tasks on it
    With EAS scheduler may actively move tasks to
    big cores, while HMP holds to little cores as
    long as possible, thus making boosting cores
    unnececarry. Based on
    https://github.com/franciscofranco/OP5-5T-unified/commit/29bc88d23518d4dc76a4bade89f291a49bf29034
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -19,6 +19,7 @@
 #include <linux/input.h>
 #include <linux/moduleparam.h>
 #include <linux/slab.h>
+#include "../../kernel/sched/sched.h"
 
 static unsigned int input_boost_freq_lp = CONFIG_INPUT_BOOST_FREQ_LP;
 static unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
@@ -52,10 +53,17 @@ static struct boost_drv *boost_drv_g;
 
 static u32 get_boost_freq(struct boost_drv *b, u32 cpu)
 {
+	int runnables = 0;
+
 	if (cpumask_test_cpu(cpu, cpu_lp_mask))
 		return input_boost_freq_lp;
 
-	return input_boost_freq_hp;
+	for (cpu; cpu < NR_CPUS; ++cpu) {
+		runnables += cpu_rq(cpu)->nr_running;
+		if (runnables)
+ 			return input_boost_freq_hp;
+	}
+	return 0;
 }
 
 static u32 get_boost_state(struct boost_drv *b)
