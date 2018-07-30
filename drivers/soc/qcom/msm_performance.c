da079a33ab47fbca64a61d27334330439aa9db4a
Yaroslav Furman <yaro330@gmail.com>
2018-07-30 12:24:04 +0300

    msm_performance: turn access to max freq back
    Stock sets freq to 2.36 GHz, this isn't a bad idea.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -534,10 +534,10 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	/* CPU:value pair */
 	if (!(ntokens % 2))
 		return -EINVAL;
-
+#if 0
 	if (!is_sh(current))
 		return 0;
-
+#endif
 	cp = buf;
 
 	cpumask_clear(limit_mask);
