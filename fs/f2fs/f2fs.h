c211ee467d4c173688d70ce9e1008e5fbb316d1e
ktommy91 <kemenesi.tamas@gmail.com>
2018-08-19 17:13:53 +0200

    f2fs:fix build

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -2657,25 +2657,29 @@ static inline void *f2fs_kmalloc(struct f2fs_sb_info *sbi,
 	return kmalloc(size, flags);
 }
 
-static inline void *kvmalloc(size_t size, gfp_t flags)
+static inline void *f2fs_kzalloc(struct f2fs_sb_info *sbi,
+					size_t size, gfp_t flags)
 {
-	void *ret;
-
-	ret = kmalloc(size, flags | __GFP_NOWARN);
-	if (!ret)
-		ret = __vmalloc(size, flags, PAGE_KERNEL);
-	return ret;
+	return f2fs_kmalloc(sbi, size, flags | __GFP_ZERO);
 }
 
-static inline void *kvzalloc(size_t size, gfp_t flags)
+static inline void *f2fs_kvmalloc(struct f2fs_sb_info *sbi,
+					size_t size, gfp_t flags)
 {
-	void *ret;
-
-	ret = kzalloc(size, flags | __GFP_NOWARN);
-	if (!ret)
-		ret = __vmalloc(size, flags | __GFP_ZERO, PAGE_KERNEL);
-	return ret;
+#ifdef CONFIG_F2FS_FAULT_INJECTION
+	if (time_to_inject(sbi, FAULT_KVMALLOC)) {
+		f2fs_show_injection_info(FAULT_KVMALLOC);
+		return NULL;
+	}
+#endif
+	return kvmalloc(size, flags);
 }
+static inline void *f2fs_kvzalloc(struct f2fs_sb_info *sbi,
+					size_t size, gfp_t flags)
+{
+	return f2fs_kvmalloc(sbi, size, flags | __GFP_ZERO);
+}
+
 
 
 static inline int get_extra_isize(struct inode *inode)
