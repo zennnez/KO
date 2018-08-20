6764fd89045c82a2633f8ddca5e8098a87343e54
sudeep <rishi.gothic@gmail.com>
2018-08-20 13:28:06 +0530

    Partial Revert "dts: steps adjustment and oc bic to 2.5ghz"
    This reverts commit 839ac861487a422a429c0283cce608dd02f6d577.
    
    no hard limit min frequency

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2263,7 +2263,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 
 	scale_freq_capacity(new_policy, NULL);
 
-	policy->min = 422400;
+	policy->min = new_policy->min;
 	policy->max = new_policy->max;
 	trace_cpu_frequency_limits(policy->max, policy->min, policy->cpu);
 
